a5682c711a859c3b37a90392c82341255633e76d
Alexey Panin <panin.lesha2112@yandex.ru>
2022-04-11 22:34:17 +0300

    first commit

---
+++ Accelerometer/src/main/java/ru/mirea/panin/accelerometer/MainActivity.java
@@ -0,0 +1,58 @@
+package ru.mirea.panin.accelerometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView azimuth, pitch, roll;
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        azimuth = findViewById(R.id.textViewAzimuth);
+        pitch = findViewById(R.id.textViewPitch);
+        roll = findViewById(R.id.textViewRoll);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float valueAzimuth = sensorEvent.values[0];
+            float valuePitch = sensorEvent.values[1];
+            float valueRoll = sensorEvent.values[2];
+            azimuth.setText("Azimuth: " + valueAzimuth);
+            pitch.setText("Pitch :" + valuePitch);
+            roll.setText("Roll: " + valueRoll);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
